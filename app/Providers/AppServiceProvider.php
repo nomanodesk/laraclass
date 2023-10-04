a98c4167bbd68c6103678660809be0f955891398
Noman <noman.u.ahmed@gmail.com>
2023-10-04 21:04:23 +0600

    submit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Repository\UserRepository;
+use App\Repository\IUserRepository;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(IUserRepository::class, UserRepository::class);
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
